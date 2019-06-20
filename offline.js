a80e4a4be2bb81d50c1d77b0465c2c56611ddb7c
wiroon traisombat <wiroontraisombat2234@gmail.com>
2019-06-20 14:16:24 +0700

    runner
    runner

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1561014928,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite22-sheet0.png",
+		"images/sprite22-sheet1.png",
+		"images/sprite22-sheet2.png",
+		"images/sprite22-sheet3.png",
+		"images/sprite22-sheet4.png",
+		"images/tiledbackground12.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
